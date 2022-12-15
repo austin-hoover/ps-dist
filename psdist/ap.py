b2aee8245f4560417eae47f7397c7bbf28277af0
austin-hoover <hooveram@ornl.gov>
2022-12-15 15:53:33 -0500

    Documentation for ap module

--- psdist/ap.py
+++ psdist/ap.py
@@ -1,10 +1,24 @@
 """Accelerator physics functions."""
 import numpy as np
-from numpy import linalg as la
 
 
+# 1D (uncoupled) Courant-Snyder parameterization
+# --------------------------------------------------------------------------------
 def twiss_2x2(sigma):
-    """Return rms Twiss parameters from u-u' covariance matrix."""
+    """RMS Twiss parameters from 2 x 2 covariance matrix.
+    
+    Parameters
+    ----------
+    sigma : ndaray, shape (2, 2)
+        The covariance matrix for position u and momentum u' [[<uu>, <uu'>], [<uu'>, <u'u'>]].
+        
+    Returns
+    -------
+    alpha : float
+        The alpha parameter (-<uu'> / sqrt(<uu><u'u'> - <uu'>^2)).
+    beta : float
+        The beta parameter (<uu> / sqrt(<uu><u'u'> - <uu'>^2)).
+    """
     eps = emittance_2x2(sigma)
     beta = sigma[0, 0] / eps
     alpha = -sigma[0, 1] / eps
@@ -12,12 +26,34 @@ def twiss_2x2(sigma):
 
 
 def emittance_2x2(sigma):
-    """Return rms emittance from u-u' covariance matrix."""
-    return np.sqrt(la.det(sigma))
+    """RMS emittance from u-u' covariance matrix.
+    
+    Parameters
+    ----------
+    sigma : ndaray, shape (2, 2)
+        The covariance matrix for position u and momentum u' [[<uu>, <uu'>], [<uu'>, <u'u'>]].
+        
+    Returns
+    -------
+    float
+        The RMS emittance (sqrt(<uu><u'u'> - <uu'>^2)).
+    """
+    return np.sqrt(np.linalg.det(sigma))
 
 
 def apparent_emittance(Sigma):
-    """Return eps_x, eps_y, eps_z."""
+    """RMS apparent emittances from 2n x 2n covariance matrix.
+    
+    Parameters
+    ----------
+    Sigma : ndarray, shape (2n, 2n)
+        A covariance matrix. (Dimensions ordered {x, x', y, y', ...}.)
+    
+    Returns
+    -------
+    eps_x, eps_y, eps_z, ... : float
+        The emittance in each phase-plane (eps_x, eps_y, eps_z, ...)
+    """
     emittances = []
     for i in range(0, Sigma.shape[0], 2):
         emittances.append(emittance_2x2(Sigma[i : i + 2, i : i + 2]))
@@ -27,7 +63,18 @@ def apparent_emittance(Sigma):
 
 
 def twiss(Sigma):
-    """Return alpha_x, beta_x, alpha_y, beta_y, ..."""
+    """RMS Twiss parameters from 2n x 2n covariance matrix.
+    
+    Parameters
+    ----------
+    Sigma : ndarray, shape (2n, 2n)
+        A covariance matrix. (Dimensions ordered {x, x', y, y', ...}.)
+    
+    Returns
+    -------
+    alpha_x, beta_x, alpha_y, beta_y, alpha_z, beta_z, ... : float
+        The Twiss parameters in each plane.
+    """
     n = Sigma.shape[0] // 2
     params = []
     for i in range(n):
@@ -37,18 +84,30 @@ def twiss(Sigma):
 
 
 def rotation_mat(angle):
-    """2x2 clockwise rotation matrix."""
+    """2 x 2 clockwise rotation matrix (angle in radians)."""
     c, s = np.cos(angle), np.sin(angle)
     return np.array([[c, s], [-s, c]])
 
 
 def rotation_mat_4x4(angle):
-    """4x4 matrix to rotate [x, x', y, y'] clockwise in the x-y plane."""
+    """4 x 4 matrix to rotate [x, x', y, y'] clockwise in the x-y plane (angle in radians)."""
     c, s = np.cos(angle), np.sin(angle)
     return np.array([[c, 0, s, 0], [0, c, 0, s], [-s, 0, c, 0], [0, -s, 0, c]])
 
 
 def phase_adv_mat(*phase_advances):
+    """Phase advance matrix (clockwise rotation in each phase plane).
+    
+    Parameters
+    ---------
+    mu1, mu2, ..., mun : float
+        The phase advance in each plane.
+    
+    Returns
+    -------
+    ndarray, shape (2n, 2n)
+        Matrix which rotates x-x', y-y', z-z', etc. by the phase advances.
+    """
     n = len(phase_advances)
     R = np.zeros((2 * n, 2 * n))
     for i, phase_advance in enumerate(phase_advances):
@@ -58,13 +117,36 @@ def phase_adv_mat(*phase_advances):
 
 
 def norm_mat_2x2(alpha, beta):
-    """Normalization matrix for u-u'."""
+    """2 x 2 normalization matrix for u-u'.
+    
+    Parameters
+    ----------
+    alpha : float
+        The alpha parameter.
+    beta : float
+        The beta parameter.
+    
+    Returns
+    -------
+    V : ndarray, shape (2, 2)
+        Matrix that transforms the ellipse defined by alpha/beta to a circle.
+    """
     return np.array([[beta, 0.0], [-alpha, 1.0]]) / np.sqrt(beta)
 
 
 def norm_mat(*twiss_params):
-    """Order is (alpha_x, beta_x, alpha_y, beta_y, alpha_z, beta_z).
-    Leave out the dimensions you don't want."""
+    """2n x 2n block-diagonal normalization matrix from Twiss parameters.
+        
+    Parameters
+    ----------
+    alpha_x, beta_x, alpha_y, beta_y, alpha_z, beta_z, ... : float
+        Twiss parameters for each dimension.
+        
+    Returns
+    -------
+    V : ndarray, shape (2n, 2n)
+        Block-diagonal normalization matrix.
+    """
     n = len(twiss_params) // 2
     V = np.zeros((2 * n, 2 * n))
     for i in range(n):
@@ -73,6 +155,13 @@ def norm_mat(*twiss_params):
     return V
 
 
+# Generalized (coupled) parameterizations
+# --------------------------------------------------------------------------------
+
+
+
+# Other
+# --------------------------------------------------------------------------------
 def lorentz_factors(mass=1.0, kin_energy=1.0):
     """Return relativistic factors gamma and beta.
 
